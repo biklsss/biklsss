30ab42da60bd794aa462f92d38ba46bdcbe73e91
biklsss <artemmayakov@bk.ru>
2024-10-18 18:44:00 +0300

    Add files via upload

--- calc2.js
+++ calc2.js
@@ -1,44 +1,97 @@
-window.addEventListener("DOMContentLoaded", function () {
-    const goodsPriceList =
-    {"ch1": 10, "ch2": 25, "ch3": 300};
-    const goodsCategory =
-    {"ch1": 1, "ch2": 2, "ch3": 3};
+function updatePrice() {
+  // Находим select по имени в DOM.
+  let s = document.getElementsByName("prodType");
+  let select = s[0];
+  let price = 0;
+  let prices = getPrices();
+  let priceIndex = parseInt(select.value) - 1;
+  if (priceIndex >= 0) {
+    price = prices.prodTypes[priceIndex];
+  }
+  
+  // Скрываем или показываем радиокнопки.
+  let radioDiv = document.getElementById("radios");
+  radioDiv.style.display = (select.value == "3" ? "block" : "none");
+  
+  // Смотрим какая товарная опция выбрана.
+  let radios = document.getElementsByName("prodOptions");
+  radios.forEach(function(radio) {
+    if (radio.checked) {
+      let optionPrice = prices.prodOptions[radio.value];
+      if (optionPrice !== undefined) {
+        price += optionPrice;
+      }
+    }
+  });
 
-    let amount = document.getElementById("amount");
-    let product = document.getElementById("goodsList");
-    let calcButton = document.getElementById("calculate");
-    let resultfield = document.getElementById("result");
-    let BlockA = document.getElementById("BlockA");
-    let BlockB = document.getElementById("BlockB");
-    let BlockC = document.getElementById("BlockC");
-    
-    calcButton.addEventListener("click", function () {
-        let inp = amount.value;
-        if (inp.match(/^\d+$/) !== null) {
-            let res = goodsPriceList[product.value] * inp;
-            resultfield.innerText = res;
-        } else {
-            resultfield.innerText = "Неккоретный ввод. Введите численное значение.";
-        }
+  // Скрываем или показываем чекбоксы.
+  let checkDiv = document.getElementById("checkboxes");
+  checkDiv.style.display = (select.value == "3" ? "none" : "block");
+
+  // Смотрим какие товарные свойства выбраны.
+  let checkboxes = document.querySelectorAll("#checkboxes input");
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      let propPrice = prices.prodProperties[checkbox.name];
+      if (propPrice !== undefined) {
+        price += propPrice;
+      }
+    }
+  });
+  
+  let prodPrice = document.getElementById("prodPrice");
+  prodPrice.innerHTML = price + " рублей";
+}
+
+function getPrices() {
+  return {
+    prodTypes: [10, 300, 25],
+    prodOptions: {
+      option2: 10,
+      option3: 5,
+    },
+    prodProperties: {
+      prop1: 30,
+      prop2: 50,
+    }
+  };
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  // Скрываем радиокнопки.
+  let radioDiv = document.getElementById("radios");
+  radioDiv.style.display = "none";
+  
+  // Находим select по имени в DOM.
+  let s = document.getElementsByName("prodType");
+  let select = s[0];
+  // Назначаем обработчик на изменение select.
+  select.addEventListener("change", function(event) {
+    let target = event.target;
+    console.log(target.value);
+    updatePrice();
+  });
+  
+  // Назначаем обработчик радиокнопок.  
+  let radios = document.getElementsByName("prodOptions");
+  radios.forEach(function(radio) {
+    radio.addEventListener("change", function(event) {
+      let r = event.target;
+      console.log(r.value);
+      updatePrice();
     });
-    product.addEventListener("change", function () {
-        if (goodsCategory[product.value]===1) {
-            calculate();
-            BlockA.style = "display:flex; flex-direction: column";
-            BlockB.style = "display:none";
-            BlockC.style = "display:none";
-        }
-        if (goodsCategory[product.value]===2) {
-            calculate();
-            BlockA.style = "display:none";
-            BlockB.style = "display:flex; flex-direction: column";
-            BlockC.style = "display:none";
-        }
-        if (goodsCategory[product.value]===3) {
-            calculate();
-            BlockA.style = "display:none";
-            BlockB.style = "display:none";
-            BlockC.style = "display:flex; flex-direction: column";
-        }
+  });
+
+    // Назначаем обработчик радиокнопок.  
+  let checkboxes = document.querySelectorAll("#checkboxes input");
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener("change", function(event) {
+      let c = event.target;
+      console.log(c.name);
+      console.log(c.value);
+      updatePrice();
     });
+  });
+
+  updatePrice();
 });
